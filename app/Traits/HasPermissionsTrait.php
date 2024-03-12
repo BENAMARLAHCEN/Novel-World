e7b9d76d83a10f0fa12ff713b61cb5ea26e6b7a1
lahcen <lahcen.ben3@gmail.com>
2024-03-12 14:45:33 +0000

    Add HasPermissionsTrait to User model

---
+++ app/Traits/HasPermissionsTrait.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Role;
+use App\Models\Permission;
+
+trait HasPermissionsTrait
+{
+    public function givePermissionsTo(...$permissions)
+    {
+        $permissions = $this->getAllPermissions($permissions);
+        if ($permissions === null) {
+            return $this;
+        }
+        $this->permissions()->saveMany($permissions);
+        return $this;
+    }
+
+    public function withdrawPermissionsTo(...$permissions)
+    {
+        $permissions = $this->getAllPermissions($permissions);
+        $this->permissions()->detach($permissions);
+        return $this;
+    }
+
+    public function refreshPermissions(...$permissions)
+    {
+        $this->permissions()->detach();
+        return $this->givePermissionsTo($permissions);
+    }
+
+    public function hasPermissionTo($permission)
+    {
+        return $this->hasPermissionThroughRole($permission) || $this->hasPermission($permission);
+    }
+
+    public function hasPermissionThroughRole($permission)
+    {
+        foreach ($permission->roles as $role) {
+            if ($this->roles->contains($role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasRole(...$roles)
+    {
+        foreach ($roles as $role) {
+            if ($this->roles->contains('slug', $role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function assignRole(...$roles)
+    {
+        $roles = $this->getAllRoles($roles);
+        if ($roles === null) {
+            return $this;
+        }
+        $this->roles()->saveMany($roles);
+        return $this;
+    }
+
+    public function getAllRoles(array $roles)
+    {
+        return Role::whereIn('name', $roles)->get();
+    }
+
+
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'users_roles');
+    }
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'users_permissions');
+    }
+
+    protected function hasPermission($permission)
+    {
+        return (bool) $this->permissions->where('slug', $permission->slug)->count();
+    }
+
+    protected function getAllPermissions(array $permissions)
+    {
+        return Permission::whereIn('slug', $permissions)->get();
+    }
+}
