c67a7f7e665021b64d1c78fb3bab9a7e858dd2b4
lahcen <lahcen.ben3@gmail.com>
2024-03-15 09:37:25 +0000

    Add RoleSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([RoleSeeder::class]);
+        
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
