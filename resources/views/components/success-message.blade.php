e57b76bd6dbb83f1a5c3476551bc48714971e991
lahcen <lahcen.ben3@gmail.com>
2024-03-19 11:34:31 +0000

    Add new blade components for skills, error message, and success message

---
+++ resources/views/components/success-message.blade.php
@@ -0,0 +1,19 @@
+@if ($message = Session::get('success'))
+            <script>
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                    }
+                });
+                Toast.fire({
+                    icon: "success",
+                    title: "{{ $message }}",
+                });
+            </script>
+        @endif
