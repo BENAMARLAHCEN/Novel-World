0d34b064532cd8b3f02d1349f456e22b5f243c58
lahcen <lahcen.ben3@gmail.com>
2024-03-22 14:53:10 +0000

    Refactor RankingController to use RankingService

--- app/Http/Controllers/Admin/RankingController.php
+++ app/Http/Controllers/Admin/RankingController.php
@@ -3,17 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Rankings\StoreRequest;
+use App\Http\Requests\Rankings\UpdateRequest;
 use App\Models\ranking;
-use Illuminate\Http\Request;
+use App\Services\RankingService;
 
 class RankingController extends Controller
 {
+
+    protected $rankingService;
+
+    public function __construct(RankingService $rankingService)
+    {
+        $this->rankingService = $rankingService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $rankings = $this->rankingService->all(10);
+        return view('admin.rankings.index', compact('rankings'));
     }
 
     /**
@@ -21,15 +32,16 @@ class RankingController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.rankings.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        //
+        $this->rankingService->create($request->validated());
+        return redirect()->route('rankings.index')->with('success', 'Ranking created successfully');
     }
 
     /**
@@ -37,7 +49,7 @@ class RankingController extends Controller
      */
     public function show(ranking $ranking)
     {
-        //
+        return view('admin.rankings.show', compact('ranking'));
     }
 
     /**
@@ -45,15 +57,16 @@ class RankingController extends Controller
      */
     public function edit(ranking $ranking)
     {
-        //
+        return view('admin.rankings.edit', compact('ranking'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, ranking $ranking)
+    public function update(UpdateRequest $request, ranking $ranking)
     {
-        //
+        $this->rankingService->update($request->validated(), $ranking->id);
+        return redirect()->route('rankings.index')->with('success', 'Ranking updated successfully');
     }
 
     /**
@@ -61,6 +74,7 @@ class RankingController extends Controller
      */
     public function destroy(ranking $ranking)
     {
-        //
+        $this->rankingService->delete($ranking->id);
+        return redirect()->route('rankings.index')->with('success', 'Ranking deleted successfully');
     }
 }
