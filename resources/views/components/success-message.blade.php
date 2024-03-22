fdc4719ab8b2638b1b9d310c3b4e11a7a7a27592
lahcen <lahcen.ben3@gmail.com>
2024-03-22 10:39:58 +0000

    Refactor success message component

--- resources/views/components/success-message.blade.php
+++ resources/views/components/success-message.blade.php
@@ -1,19 +1,7 @@
 @if ($message = Session::get('success'))
-            <script>
-                const Toast = Swal.mixin({
-                    toast: true,
-                    position: "top-end",
-                    showConfirmButton: false,
-                    timer: 3000,
-                    timerProgressBar: true,
-                    didOpen: (toast) => {
-                        toast.onmouseenter = Swal.stopTimer;
-                        toast.onmouseleave = Swal.resumeTimer;
-                    }
-                });
-                Toast.fire({
-                    icon: "success",
-                    title: "{{ $message }}",
-                });
-            </script>
-        @endif
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            NioApp.Toast('{{ $message }}', 'success');
+        });
+    </script>
+@endif
