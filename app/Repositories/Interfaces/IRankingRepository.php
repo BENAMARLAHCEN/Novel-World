f38dd620d710a493f80799bcd5656bfc04f8a719
lahcen <lahcen.ben3@gmail.com>
2024-03-22 14:52:08 +0000

    Add paginate method to IRankingRepository interface

--- app/Repositories/Interfaces/IRankingRepository.php
+++ app/Repositories/Interfaces/IRankingRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories\Interfaces;
 interface IRankingRepository
 {
     public function all();
+    public function paginate(int $perPage = 10);
     public function findById(int $id);
     public function create(array $attributes);
     public function update(int $id, array $attributes);
