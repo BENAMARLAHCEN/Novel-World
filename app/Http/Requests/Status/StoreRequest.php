91486613ba82a08d285f17aa82c2919873777fc0
lahcen <lahcen.ben3@gmail.com>
2024-03-22 14:44:52 +0000

    Add form request classes for Novels, Rankings, and Status

---
+++ app/Http/Requests/Status/StoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Status;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255|unique:status,name',
+            'color' => 'required|string|max:255',
+            'background_color' => 'required|string|max:255',
+            'border_color' => 'required|string|max:255',
+            'text_color' => 'required|string|max:255',
+            'icon' => 'required|string|max:255',
+        ];
+    }
+}
