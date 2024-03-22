bee64fb13ede02c55c1a1ac5483c84e37b3f56ca
lahcen <lahcen.ben3@gmail.com>
2024-03-22 10:55:20 +0000

    Add StatuController, Statu model, and create_status_table migration

---
+++ app/Http/Controllers/StatuController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Statu;
+use Illuminate\Http\Request;
+
+class StatuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Statu $statu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Statu $statu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Statu $statu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Statu $statu)
+    {
+        //
+    }
+}
