0882a3084e562db54d16d0e1ff637b6e8649af86
lahcen <lahcen.ben3@gmail.com>
2024-04-04 12:28:49 +0000

    admin and author model test

---
+++ app/Models/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+// admin model for admin data manipulation
+// Path: app/Models/Admin.php
+// admin model extends the user model to inherit the user model properties
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
